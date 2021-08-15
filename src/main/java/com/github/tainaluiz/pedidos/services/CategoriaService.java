8b1fb33ba60422d345405097766fbede05a27713
Tainã Luiz <tluuiiz@gmail.com>
2021-08-15 14:25:58 -0300

    Método DELETE para Categoria

--- src/main/java/com/github/tainaluiz/pedidos/services/CategoriaService.java
+++ src/main/java/com/github/tainaluiz/pedidos/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.github.tainaluiz.pedidos.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.github.tainaluiz.pedidos.domain.Categoria;
 import com.github.tainaluiz.pedidos.repositories.CategoriaRepository;
+import com.github.tainaluiz.pedidos.services.exceptions.DataIntegrityException;
 import com.github.tainaluiz.pedidos.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,5 +27,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) throws ObjectNotFoundException {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Long id) throws DataIntegrityException {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 }
